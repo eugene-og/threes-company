1ace321884158321eecc2b2b4e1926d7ed8129ac
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-10-23 19:55:13 +0000

    * Clarify the way turn structure works (the client can support multiple players) * Add some TODOs for GUI<->Logic hooks

--- java/src/edu/columbia/threescompany/game/BlobMove.java
+++ java/src/edu/columbia/threescompany/game/BlobMove.java
@@ -1,5 +1,12 @@
 package edu.columbia.threescompany.game;
 
 public class BlobMove {
-
+	// TODO
+	/* As I see it, this will be some sort of sequence, say
+	 * private List<MoveComponent> _parts;
+	 * 
+	 * and then
+	 * 
+	 * MoveComponent --> ActivationMove | PhysicalMove
+	 */ 
 }
--- java/src/edu/columbia/threescompany/game/GameMove.java
+++ java/src/edu/columbia/threescompany/game/GameMove.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameMove {
-
 	public List<BlobMove> getBlobMoves() {
-		// TODO Auto-generated method stub
+		// TODO this doesn't do anything useful yet
 		return new ArrayList<BlobMove>();
 	}
-
 }
