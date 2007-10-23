1a41440dc006992e7f96c491f41ee2edfe068871
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-10-23 21:00:36 +0000

    add TODOs

--- java/src/edu/columbia/threescompany/client/communication/ServerConnection.java
+++ java/src/edu/columbia/threescompany/client/communication/ServerConnection.java
@@ -7,12 +7,13 @@ public class ServerConnection {
 	public ServerMessage receiveMessage() {
 		/* Block until a message is received from the server, then return it.
 		 * If the game has ended, return null. */
-		// TODO
+		// TODO Eugene, I have to discuss the exact workings of this with you -- Zach
 		return null;
 	}
 
 	public void sendMove(GameMove move, LocalGameState state) {
-		// TODO Auto-generated method stub
+		// TODO Eugene, I have to discuss the exact workings of this with you -- Zach
+		
 		// The server needs to propagate this move to all clients, who
 		// then animate it. Clients need to agree with "state", so we're
 		// passing that too, also to be serialized.
