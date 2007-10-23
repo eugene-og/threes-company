106e1a70eaff586cf1813bfa069dd071d838c202
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-10-23 21:39:31 +0000

    Add anchor points

---
+++ java/src/edu/columbia/threescompany/gameobjects/AnchorPoint.java
@@ -0,0 +1,35 @@
+package edu.columbia.threescompany.gameobjects;
+
+import edu.columbia.threescompany.common.Coordinate;
+
+public class AnchorPoint extends ImmovableGameObject {
+	private static final long serialVersionUID = 2365645717684731802L;
+
+	public AnchorPoint(double x, double y, double radius) {
+		_position = new Coordinate(x, y);
+		this._radius = 0;
+	}	
+	
+	public void die() { /* I AM IMMUNE TO DEATH */ }
+	public void grow() { /* I AM IMMUNE TO GROWTH */ }
+
+	public Force actOn(GameObject obj) {
+		return Force.NULL_FORCE;
+	}
+
+	public boolean isDead() {
+		return false;
+	}
+
+	public void checkCollision(GameObject rhs) {
+		if (!(rhs instanceof Blob)) return;	/* We don't care. */
+		Blob blob = (Blob) rhs;
+		
+		blob.setAnchored(collidesWith(blob));
+	}
+
+	private boolean collidesWith(Blob blob) {
+		return _position.distanceFrom(blob.getPosition()) < blob.getRadius();
+	}
+
+}
