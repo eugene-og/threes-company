cd443f167ac54f43a8fb1e7afa565606b671ced9
morales.john <morales.john@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-10-24 07:57:10 +0000

    * Added version, images_dir, images_menu_dir, help_about

--- trunk/java/src/edu/columbia/threescompany/graphics/GuiConstants.java
+++ trunk/java/src/edu/columbia/threescompany/graphics/GuiConstants.java
@@ -2,16 +2,32 @@ package edu.columbia.threescompany.graphics;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.File;
 
 public class GuiConstants {
 
-	public static final int BOARD_LENGTH = 400;
+	public static final String	VERSION = "0.1";
 	
-	public static final int GUI_WIDTH = 640;
-	public static final int GUI_HEIGHT = 480;
+	public static final int		BOARD_LENGTH = 400;
 	
-	public static final Font CHAT_FONT = new Font("Tahoma", Font.PLAIN, 9);
+	public static final int		GUI_WIDTH = 640;
 	
-	public static final Color BG_COLOR = Color.WHITE;
+	public static final int		GUI_HEIGHT = 480;
 	
+	public static final Font	CHAT_FONT = new Font("Tahoma", Font.PLAIN, 9);
+	
+	public static final Color	BG_COLOR = Color.WHITE;
+	
+	public static final String	IMAGES_DIR = "images" + File.separator;
+	
+	public static final String	IMAGES_MENU_DIR = "images" + File.separator + "menu" + File.separator;
+	
+    /** String for About box. */
+    public static final String	HELP_ABOUT =	"<html><b>Blobs v" + VERSION + "</b>\n" +
+												"2007, Columbia University, CS4995\n" +
+												"Daniel Benamy\n" +
+												"John Morales\n" +
+												"Eugene Kozhukalo\n" +
+    											"Zach van Schouwen";
 }
+												
