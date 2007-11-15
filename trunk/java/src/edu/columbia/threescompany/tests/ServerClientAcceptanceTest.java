01750b9ffef71f1613ce8e8d9f044745c8dd7b89
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-15 01:21:19 +0000

    Clarify missing server connection hooks

---
+++ trunk/java/src/edu/columbia/threescompany/tests/ServerClientAcceptanceTest.java
@@ -0,0 +1,32 @@
+package edu.columbia.threescompany.tests;
+
+import edu.columbia.threescompany.client.BlobsClient;
+import edu.columbia.threescompany.server.BlobsServer;
+import edu.columbia.threescompany.tests.BaseTestCase;
+
+public class ServerClientAcceptanceTest extends BaseTestCase {
+	public void testBasicFunctionality() throws Exception {		
+		final int TEST_PORT = 3444;
+		
+		Thread serverThread = new Thread(new Runnable() {
+				public void run() {
+					BlobsServer.main(new String[] { String.valueOf(TEST_PORT) });
+				};
+		});
+		
+		Thread clientThread = new Thread(new Runnable() {
+			public void run() {
+				try {
+					BlobsClient.main(new String[] { String.valueOf(TEST_PORT) });
+				} catch (Exception exception) {
+					throw new RuntimeException(exception);
+				}
+			};
+		});
+		
+		serverThread.run();
+		clientThread.run();
+		
+		
+	}
+}
