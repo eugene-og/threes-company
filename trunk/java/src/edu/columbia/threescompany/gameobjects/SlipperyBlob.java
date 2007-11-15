ae006dc34d9cc552bd03cd4b10f3188eb5073b53
morales.john <morales.john@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-15 04:39:54 +0000

    * constructors made public * added constructor using default min size

--- trunk/java/src/edu/columbia/threescompany/gameobjects/SlipperyBlob.java
+++ trunk/java/src/edu/columbia/threescompany/gameobjects/SlipperyBlob.java
@@ -4,8 +4,12 @@ import edu.columbia.threescompany.common.Force;
 import edu.columbia.threescompany.game.Player;
 
 public class SlipperyBlob extends Blob {
-	protected SlipperyBlob(double x, double y, double radius, Player owner) {
+	public SlipperyBlob(double x, double y, double radius, Player owner) {
 		super(x, y, radius, owner);
+	}
+	
+	public SlipperyBlob(double x, double y, Player owner) {
+		super(x, y, owner);
 	}
 
 	private static final long serialVersionUID = 8421390274567998819L;
