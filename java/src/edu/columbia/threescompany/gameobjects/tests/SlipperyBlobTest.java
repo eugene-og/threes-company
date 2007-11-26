cb95cfd7bf7c4ad2f0faf08cb579cfed9162e48b
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-26 00:02:27 +0000

    Add tests for missing game objects

---
+++ java/src/edu/columbia/threescompany/gameobjects/tests/SlipperyBlobTest.java
@@ -0,0 +1,9 @@
+package edu.columbia.threescompany.gameobjects.tests;
+
+import edu.columbia.threescompany.tests.BaseTestCase;
+
+public class SlipperyBlobTest extends BaseTestCase {
+	public void testSlipperyBlobs() {
+		fail("Not yet implemented!");
+	}
+}
---
+++ java/src/edu/columbia/threescompany/gameobjects/tests/SpringTest.java
@@ -0,0 +1,9 @@
+package edu.columbia.threescompany.gameobjects.tests;
+
+import edu.columbia.threescompany.tests.BaseTestCase;
+
+public class SpringTest extends BaseTestCase {
+	public void testSprings() {
+		fail("Springs not implemented");
+	}
+}
