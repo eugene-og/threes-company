654bbb0d5d54245a72c2f42f75e6c2662653dac8
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-26 03:59:33 +0000

    dumb network fix

--- trunk/java/src/edu/columbia/threescompany/client/communication/MoveStatePair.java
+++ trunk/java/src/edu/columbia/threescompany/client/communication/MoveStatePair.java
@@ -1,9 +1,12 @@
 package edu.columbia.threescompany.client.communication;
 
+import java.io.Serializable;
+
 import edu.columbia.threescompany.client.LocalGameState;
 import edu.columbia.threescompany.game.GameMove;
 
-public class MoveStatePair {
+public class MoveStatePair implements Serializable {
+	private static final long serialVersionUID = -1917229923350700923L;
 	public MoveStatePair(GameMove move, LocalGameState state) {
 		_move = move;
 		_state = state;
