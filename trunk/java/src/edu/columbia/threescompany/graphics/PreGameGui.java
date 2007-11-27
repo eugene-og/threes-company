09695577705e60d59c2a37dba2a936749ca32f19
morales.john <morales.john@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-27 19:39:34 +0000

    * pretty up pregame gui

--- trunk/java/src/edu/columbia/threescompany/graphics/PreGameGui.java
+++ trunk/java/src/edu/columbia/threescompany/graphics/PreGameGui.java
@@ -1,15 +1,20 @@
 package edu.columbia.threescompany.graphics;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import edu.columbia.threescompany.client.GameType;
 
@@ -23,33 +28,57 @@ public class PreGameGui extends JFrame {
 	private GameType _gameType = null;
 	
 	private PreGameGui() {
+		super("Welcome to Blobs");
 		JPanel mainPane = (JPanel)this.getContentPane();
-		mainPane.setLayout(new GridLayout(4, 1));
-		mainPane.add(new JLabel("Blobs"));
+		mainPane.setLayout(new GridLayout(5, 1));
 		
-		addGameButton(mainPane, "Network", GameType.NETWORK);
-		addGameButton(mainPane, "Hotseat", GameType.HOTSEAT);
+//		JLabel label = new JLabel("", SwingConstants.CENTER);
+//		label.setFont(new Font("Tahoma", Font.BOLD, 14));
+//		mainPane.add(label);
+		mainPane.add(new JLabel(""));
+		
+		mainPane.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8), 
+						   BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder("Pre-game Setup"), 
+															  BorderFactory.createEmptyBorder(5, 5, 5, 5))));
+		
+		JPanel contentPane = new JPanel(new BorderLayout());
+		
+		addGameButton(contentPane, "Network", "Challenge a player over the internet", GameType.NETWORK);
+		mainPane.add(contentPane);
+		mainPane.add(new JLabel(""));
+		
+		contentPane = new JPanel(new BorderLayout());
+		addGameButton(contentPane, "Hotseat", "Switch between turns", GameType.HOTSEAT);
+		
+		mainPane.add(contentPane);
+		mainPane.add(new JLabel(""));
 		
         // Get coordinates such that window's centered on screen
 		Rectangle rect = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-		int xPos = (int)(rect.getWidth() - getPreferredSize().width)/2;
-		int yPos = (int)(rect.getHeight() - getPreferredSize().height)/2;
+		int xPos = (int)(rect.getWidth() - GuiConstants.PREGAME_GUI_WIDTH)/2;
+		int yPos = (int)(rect.getHeight() - GuiConstants.PREGAME_GUI_HEIGHT)/2;
 		
+		setPreferredSize(new Dimension(GuiConstants.PREGAME_GUI_WIDTH, GuiConstants.PREGAME_GUI_HEIGHT));
 		setLocation(xPos, yPos);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
 	}
 	
-	private void addGameButton(JPanel pane, String name, final GameType type) {
+	private void addGameButton(JPanel pane, String name, String description, final GameType type) {
 		JButton btn = new JButton(name);
+		btn.setPreferredSize(new Dimension(100,10));
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				_gameType = type;
 				dispose();
 			}
 		});
-		pane.add(btn);
+		pane.add(btn, BorderLayout.WEST);
+		
+		JLabel label = new JLabel(description);
+		label.setFont(GuiConstants.CHAT_FONT);
+		pane.add(label, BorderLayout.EAST);
 	}
 	
 	public static GameType getGameType() {
