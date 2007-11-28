dd04f32ee26f78470f8a68d2b0997c0c1e80d88b
dbenamy <dbenamy@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-28 02:21:15 +0000

    * More stubs for new connection/auth stuff.

--- trunk/java/src/edu/columbia/threescompany/client/communication/PlayerNameInUseException.java
+++ trunk/java/src/edu/columbia/threescompany/client/communication/PlayerNameInUseException.java
@@ -1,6 +1,6 @@
 package edu.columbia.threescompany.client.communication;
 
-public class InvalidPlayerNameException extends Exception {
+public class PlayerNameInUseException extends Exception {
 
 	private static final long serialVersionUID = -3335342979499119161L;
 
