34c1b707321f9d6199906ad4ae325d3b40fc2320
dbenamy <dbenamy@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-11-28 08:31:10 +0000

    * Sanitize blobs sizes.

--- java/src/edu/columbia/threescompany/gameobjects/GameParameters.java
+++ java/src/edu/columbia/threescompany/gameobjects/GameParameters.java
@@ -1,8 +1,8 @@
 package edu.columbia.threescompany.gameobjects;
 
 public class GameParameters {
-	public static final double BLOB_SIZE_LIMIT = 3;
-	public static final double BLOB_INITIAL_SIZE = 1;
+	public static final double BLOB_SIZE_LIMIT = 1.5;
+	public static final double BLOB_INITIAL_SIZE = 0.5;
 	public static final double BLOB_GROWTH_FACTOR = 1.095;
 	
 	public static final double DEATH_RAY_RANGE_MULTIPLIER = 3;
