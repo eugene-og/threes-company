2bf9f112979627b3ba7eaba2b7c768c8e3e5b508
genek81 <genek81@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-01 17:01:25 +0000

    major authentication update

---
+++ trunk/java/src/edu/columbia/threescompany/client/communication/AuthenticationException.java
@@ -0,0 +1,11 @@
+package edu.columbia.threescompany.client.communication;
+
+public class AuthenticationException extends Exception {
+
+	public AuthenticationException(String msg) {
+		super(msg);
+	}
+
+	private static final long serialVersionUID = -3743064271883702834L;
+
+}
--- trunk/java/src/edu/columbia/threescompany/server/BlobsServerQuickAuthenticator.java
+++ trunk/java/src/edu/columbia/threescompany/server/BlobsServerQuickAuthenticator.java
@@ -7,58 +7,84 @@ import org.quickserver.net.server.DataMode;
 import org.quickserver.net.server.DataType;
 import org.quickserver.net.server.QuickAuthenticator;
 
+import edu.columbia.threescompany.client.communication.AuthenticationObject;
 import edu.columbia.threescompany.game.Player;
 
 public class BlobsServerQuickAuthenticator extends QuickAuthenticator {
+	private String _errorMsg;
+	
 	public boolean askAuthorisation(ClientHandler clientHandler) throws IOException {
 		BlobsGameState gameState = BlobsGameState.instance();
-		Object[] players;
+		AuthenticationObject ao;
 		clientHandler.setDataMode(DataMode.OBJECT, DataType.IN);
 		try {
-			players = (Object[]) askObjectInput(clientHandler, null);
+			ao = (AuthenticationObject) askObjectInput(clientHandler, null);
 		} catch (Exception e) {
 			throw new RuntimeException("Error deserializing player list", e);
 		}
 		
-		boolean retval = true;
+		Object[] players = (Object[])ao.getPlayers();
+		boolean isFirstConnection = ao.isFirstConnection();
 		
-		for (int i = 0; i < players.length; i++) {
-			retval &= authenticatePlayer((Player) players[i], clientHandler, gameState);
+		if (!isFirstConnection) {
+			updatePlayerServerData(players, clientHandler, gameState);
+			return true;
+		}
+		
+		boolean retval = true;
+		_errorMsg = "Auth Failed";
+		
+		if (players.length == 2 && (((Player) players[0]).getName().equals(((Player) players[1]).getName()))) {
+			_errorMsg = "Auth Failed - Duplicate usernames in Hotseat mode";
+			retval = false;
+		} else {
+			for (int i = 0; i < players.length; i++) {
+				retval &= authenticatePlayer((Player)players[i], clientHandler, gameState);
+			}
+		}
+		
+		if (retval == true) {
+			sendAuthenticationMessage(clientHandler, "Auth OK");
+		} else {
+			sendAuthenticationMessage(clientHandler, _errorMsg);
 		}
 		
 		return retval;
 	}
 
-	private boolean authenticatePlayer(Player player, ClientHandler clientHandler, BlobsGameState gameState) throws IOException {
-		boolean retval = false;
-		if(player.getName() == null || player.getName().equals("")) {
-			clientHandler.setDataMode(DataMode.STRING, DataType.IN); //set String data mode for this connection
-			clientHandler.setDataMode(DataMode.STRING, DataType.OUT);
-			sendString(clientHandler, "Auth Failed - Username empty");
-			retval = false;
-		} else if (gameState.isAlreadyAuthenticated(player.getName(), clientHandler.getHostAddress())) {
-			//this is the 2nd connection for the main game loop
+	private void sendAuthenticationMessage(ClientHandler clientHandler,
+			String message) throws IOException {
+		clientHandler.setDataMode(DataMode.STRING, DataType.IN); //set String data mode for this connection
+		clientHandler.setDataMode(DataMode.STRING, DataType.OUT);
+		sendString(clientHandler, message);	
+	}
+
+	private void updatePlayerServerData(Object[] players, ClientHandler clientHandler, BlobsGameState gameState) throws IOException {
+		for (int i = 0; i < players.length; i++) {
+			Player player = (Player)players[i];
 			clientHandler.setDataMode(DataMode.OBJECT, DataType.IN);
 			clientHandler.setDataMode(DataMode.OBJECT, DataType.OUT);
 			PlayerServerData playerServerData = gameState.getPlayerServerData(player.getName());
 			playerServerData.setGameClientHandler(clientHandler);
 			playerServerData.setIsReadyToPlay(false);
-			playerServerData.addPlayer(player);
 			gameState.increasePlayerCount();
-			retval = true;
+		}
+	}
+
+	private boolean authenticatePlayer(Player player, ClientHandler clientHandler, BlobsGameState gameState) throws IOException {
+		boolean retval = false;
+		if(player.getName() == null || player.getName().equals("")) {
+			_errorMsg = "Auth Failed - Username empty";
+			retval = false;		
 		} else if (gameState.isHandleTaken(player.getName(), clientHandler.getHostAddress())) {
-			clientHandler.setDataMode(DataMode.STRING, DataType.IN); //set String data mode for this connection
-			clientHandler.setDataMode(DataMode.STRING, DataType.OUT);
-			sendString(clientHandler, "Auth Failed - Handle in use");
+			_errorMsg = "Auth Failed - Handle in use";
 			retval = false;
 		} else {
 			//this is the 1st connection from ChatThread
-			clientHandler.setDataMode(DataMode.STRING, DataType.IN); //set String data mode for this connection
-			clientHandler.setDataMode(DataMode.STRING, DataType.OUT);
-			sendString(clientHandler, "Auth OK");
 			PlayerServerData newPlayerServerData = (PlayerServerData) clientHandler.getClientData();
 			newPlayerServerData.setHostAddress(clientHandler.getHostAddress());
 			newPlayerServerData.setChatClientHandler(clientHandler);
+			newPlayerServerData.addPlayer(player);
 			gameState.addPlayerServerData(player.getName(), newPlayerServerData);
 			retval = true;
 		}
