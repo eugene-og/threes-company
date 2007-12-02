a070abd5516e631bbc2eb132b0a504744ccabf0f
morales.john <morales.john@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-02 21:58:14 +0000

    * tweak so gameplay slightly faster.  no need to make it too fast because ordinally moves won't be across the screen.

--- java/src/edu/columbia/threescompany/gameobjects/GameParameters.java
+++ java/src/edu/columbia/threescompany/gameobjects/GameParameters.java
@@ -10,7 +10,7 @@ public class GameParameters {
 	
 	/* How many pieces moves are broken down into (basically 1/dt).
 	 * I may later replace this with integrals. --ZvS */
-	public static final double GRANULARITY_OF_PHYSICS = 130;
+	public static final double GRANULARITY_OF_PHYSICS = 100;
 	
 	/* After all blobs have stopped being moved by PLAYERS, we want to sit and
 	 * let the force blobs do their thing. How long should this be?
@@ -23,5 +23,5 @@ public class GameParameters {
 	public static final double FORCE_OF_USERS_HAND = 3;
 	
 	/* How many milliseconds between average animation frames? (Variable!) */
-	public static final int AVERAGE_MS_FRAME_GAP = 10;
+	public static final int AVERAGE_MS_FRAME_GAP = 8;
 }
