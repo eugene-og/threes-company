f6f73b29965971ac3272c1e3f94fd48fdfd7c37d
morales.john <morales.john@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-03 23:39:26 +0000

    * for computing move costs

---
+++ java/src/edu/columbia/threescompany/graphics/ActionPointEngine.java
@@ -0,0 +1,44 @@
+package edu.columbia.threescompany.graphics;
+
+import edu.columbia.threescompany.common.Coordinate;
+import edu.columbia.threescompany.gameobjects.Blob;
+import edu.columbia.threescompany.gameobjects.DeathRayBlob;
+import edu.columbia.threescompany.gameobjects.ExplodingBlob;
+import edu.columbia.threescompany.gameobjects.ForceBlob;
+import edu.columbia.threescompany.gameobjects.GameParameters;
+import edu.columbia.threescompany.gameobjects.SlipperyBlob;
+
+public class ActionPointEngine {
+	
+	public static double getCostOfPhysicalMove(Blob blob, Coordinate click) {
+		return GameParameters.BASE_PHYSICAL_MOVE_COST + (blob.getPosition().distanceFrom(click) / (1/blob.getWeight()));
+	}
+	
+	public static double getCostOfProratedAction(Blob blob) {
+		double ret;
+		if (blob instanceof ForceBlob) {
+			ret = GameParameters.BASE_FORCE_COST + (blob.getWeight() / GameParameters.BASE_FORCE_COST);   
+		} else if (blob instanceof ExplodingBlob) {
+			ret = GameParameters.BASE_EXPLODE_COST + (blob.getWeight() / GameParameters.BASE_EXPLODE_COST);
+		} else {
+			throw new RuntimeException("can only perform actions on blobs");
+		}
+		
+		return ret;
+	}
+	
+	public static double getCostOfRotate() {
+		return GameParameters.BASE_ROTATE_COST;
+	}
+	
+	public static double getCostOfSplit(Blob blob) {
+		return GameParameters.BASE_SPLIT_COST;
+	}
+	
+	public static double getCostOfProjectile(Blob blob) {
+		if (!(blob instanceof DeathRayBlob) && !(blob instanceof SlipperyBlob)) {
+			throw new RuntimeException("projectile costs only apply to death ray and slippery blobs");
+		}
+		return (blob instanceof DeathRayBlob) ? GameParameters.BASE_FIRE_DEATH_RAY_COST : GameParameters.BASE_FIRE_SLIPPERY_COST;	
+	}
+}
