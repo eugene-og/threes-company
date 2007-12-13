113320a9f2dda459f01ff456cd580e511379a06b
zachvs <zachvs@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-13 00:10:45 +0000

    issue 6: Implement slippery blobs fully (also refactoring similar concerns)

--- java/src/edu/columbia/threescompany/gameobjects/ExplodingBlob.java
+++ java/src/edu/columbia/threescompany/gameobjects/ExplodingBlob.java
@@ -1,12 +1,10 @@
 package edu.columbia.threescompany.gameobjects;
 
 import edu.columbia.threescompany.client.LocalGameState;
-import edu.columbia.threescompany.common.Force;
 import edu.columbia.threescompany.game.Player;
 
-public class ExplodingBlob extends StateModifyingBlob {
+public class ExplodingBlob extends StateModifyingBlob<Hole> {
 	private static final long serialVersionUID = -5592289336246561407L;
-	private boolean _activated;
 
 	public ExplodingBlob(double x, double y, double radius, Player owner, LocalGameState state) {
 		super(x, y, radius, owner, state);
@@ -15,20 +13,16 @@ public class ExplodingBlob extends StateModifyingBlob {
 	public ExplodingBlob(double x, double y, Player owner, LocalGameState state) {
 		this(x, y, GameParameters.BLOB_INITIAL_SIZE, owner, state);
 	}
-
-	public Force actOn(GameObject obj) {
-		return Force.NULL_FORCE;
-	}
-	
-	public void activate(boolean activated) {
-		_activated = activated;
-		if (_activated) {
-			this.die();
-			_state.addObject(new Hole(this.getPosition().x, this.getPosition().y, this.getRadius() * 2));	
-		}
-	}
 	
 	public GameObject clone() {
 		return new ExplodingBlob(_position.x, _position.y, _radius, _owner, _state);
+	}
+	
+	public void activate(boolean activated) {
+		if (!activated || isDead()) return;
+		
+		this.die();
+		Hole hole = new Hole(this.getPosition().x, this.getPosition().y, this.getRadius() * 2);
+		_state.addObject(hole);	
 	}
 }
--- java/src/edu/columbia/threescompany/gameobjects/tests/SlipperyBlobTest.java
+++ java/src/edu/columbia/threescompany/gameobjects/tests/SlipperyBlobTest.java
@@ -1,9 +1,46 @@
 package edu.columbia.threescompany.gameobjects.tests;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import edu.columbia.threescompany.client.LocalGameState;
+import edu.columbia.threescompany.common.Coordinate;
+import edu.columbia.threescompany.game.GameMove;
+import edu.columbia.threescompany.game.Player;
+import edu.columbia.threescompany.game.graphics.GUIGameMove;
+import edu.columbia.threescompany.gameobjects.Blob;
+import edu.columbia.threescompany.gameobjects.GameObject;
+import edu.columbia.threescompany.gameobjects.SlipperyBlob;
+import edu.columbia.threescompany.gameobjects.SlipperySpot;
 import edu.columbia.threescompany.tests.BaseTestCase;
 
 public class SlipperyBlobTest extends BaseTestCase {
-	public void testSlipperyBlobs() {
-		fail("Not yet implemented!");
+	public void testSpotCreation() {
+		LocalGameState state = LocalGameState.getSpecifiedGameState(new ArrayList<GameObject>());
+		SlipperyBlob slipperyBlob = new SlipperyBlob(3.0, 3.0, 1.0, Player.NULL_PLAYER, state);
+		state.addObject(slipperyBlob);
+		
+		List<Blob> activation = Arrays.asList(new Blob[] { slipperyBlob });
+		GUIGameMove move = new GUIGameMove(new HashMap<Blob, Coordinate>(), activation, new ArrayList<Blob>());
+		
+		state.executeMove(new GameMove(move));
+		
+		assertTrue("Slippery blob should be dead", slipperyBlob.isDead());
+		List<GameObject> objects = state.getObjects();
+		assertEquals("Should be a new object created", 2, objects.size());
+		
+		SlipperySpot spot = null;
+		try {
+			spot = (SlipperySpot) objects.get(1);
+		} catch (ClassCastException e) {
+			fail("Object should be a hole! Was: " + objects.get(1));
+		}
+		
+		assertEquals("Position should be the same", slipperyBlob.getPosition(),
+					 spot.getPosition());
+		assertSignificantlyGreaterThan("Radius should be larger", spot.getRadius(),
+									   slipperyBlob.getRadius());
 	}
 }
