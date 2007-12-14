c95f86452a949e4c42000bcae930ab4ac582b030
dbenamy <dbenamy@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-14 02:17:28 +0000

    * Moved frame rate constant to GameParameters. * Lowered it which makes the animation loop smoother on my machine.

--- java/src/edu/columbia/threescompany/graphics/RedrawThread.java
+++ java/src/edu/columbia/threescompany/graphics/RedrawThread.java
@@ -1,9 +1,9 @@
 package edu.columbia.threescompany.graphics;
 
+import edu.columbia.threescompany.gameobjects.GameParameters;
+
 public class RedrawThread {
 	private boolean _stop = false;
-	private static final int FRAME_DELAY = 60;
-	
 	RedrawThread(final Board board) {
 		new Thread(new Runnable() {
 			public void run() {
@@ -13,7 +13,7 @@ public class RedrawThread {
 					/* No more than 5 ms, you criminal! */
 					board.repaint(5, 0, 0, board.getWidth(), board.getHeight());
 					
-					long sleepTime = FRAME_DELAY - (System.currentTimeMillis() - start);
+					long sleepTime = GameParameters.FRAME_DELAY - (System.currentTimeMillis() - start);
 						
 					try {
 						if (sleepTime > 0) Thread.sleep(sleepTime);
