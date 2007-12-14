6e29009cdcecf80ae31b095baca7966d6dd8e888
genek81 <genek81@46a2a7d0-963a-0410-8f2c-f7afc7a904d2>
2007-12-14 05:13:53 +0000

    * added settings panel * added sound on/off setting

---
+++ trunk/java/src/edu/columbia/threescompany/client/Settings.java
@@ -0,0 +1,15 @@
+package edu.columbia.threescompany.client;
+
+
+public class Settings {
+	
+	public boolean soundOn = true;
+	
+	private static Settings _instance;
+	
+	public static Settings getInstance() {
+		if (_instance == null) _instance = new Settings();
+		return _instance;
+	}
+
+}
